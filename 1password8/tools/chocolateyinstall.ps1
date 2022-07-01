e84786a23b93874b24d1ae35eafab7b77f1d3b16
oc-software <104031098+oc-software@users.noreply.github.com>
2022-07-01 17:02:45 -0600

    Add files via upload

--- 1password8/tools/chocolateyinstall.ps1
+++ 1password8/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$url        = 'https://downloads.1password.com/win/1PasswordSetup-latest.exe'
 $url64      = 'https://downloads.1password.com/win/1PasswordSetup-latest.exe'
 $getUser = (((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1])
 $env:LocalAppData = "C:\Users\$getUser.OC\AppData\Local"
@@ -11,16 +10,15 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-#  url           = $url
   url64bit      = $url64
-
+  appPath       = "$env:ProgramFiles\1Password"
   softwareName  = '1Password 8'
-
-#  checksum      = '74AB3B856FDE8B7A5C4A7602CA6C21993FE113DE40E5F92487D68B688D0DECD6'
-#  checksumType  = 'sha256'
   checksum64    = '74AB3B856FDE8B7A5C4A7602CA6C21993FE113DE40E5F92487D68B688D0DECD6'
   checksumType64= 'sha256'
   silentArgs = '--silent'
 }
 
+if (!{Test-Path "$env:ProgramFiles\1Password"}) {
+  New-Item -ItemType Directory -Path "$env:ProgramFiles\1Password"
+}
 Install-ChocolateyPackage @packageArgs
